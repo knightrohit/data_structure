eff1613012d56f5133c14bc4778b2c0963c12bef
Rohit <knight.rohit@gmail.com>
2021-09-06 16:59:28 -0500

    usng heap

---
+++ heap/kth_largest_element.py
@@ -0,0 +1,20 @@
+"""
+Time Complexity = O(NlogN)
+Space Complexity = O(N)
+"""
+
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = nums
+        self.k = k
+        heapq.heapify(self.heap)
+        
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
