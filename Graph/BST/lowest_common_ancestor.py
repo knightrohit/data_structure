e9b5b7de058460a861ae00b3e339644220951472
Rohit <knight.rohit@gmail.com>
2021-09-10 01:36:15 -0500

    using bst feature

---
+++ Graph/BST/lowest_common_ancestor.py
@@ -0,0 +1,20 @@
+"""
+Time/Space Complexity: O(N)
+
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if not root:
+            return root
+        
+        
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+            
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        else:
+            return root
